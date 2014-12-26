f5e72279be65928e5495342c8c3a68e4ad5d8180
Golo Roden <webmaster@goloroden.de>
2014-12-26 11:35:44 +0100

    Updated dependencies.

--- test/eventbusTests.js
+++ test/eventbusTests.js
@@ -6,7 +6,7 @@ var eventbus = require('../lib/eventbus');
 
 suite('eventbus', function () {
   suite('create', function () {
-    test('returns an event emitter.', function () {
+    test('returns an event emitter.', function (done) {
       var bus = eventbus.create();
 
       assert.that(bus, is.ofType('object'));
@@ -15,24 +15,27 @@ suite('eventbus', function () {
       assert.that(bus.emit, is.ofType('function'));
       assert.that(bus.removeListener, is.ofType('function'));
       assert.that(bus.removeAllListeners, is.ofType('function'));
+      done();
     });
 
-    test('does not return the global instance.', function () {
+    test('does not return the global instance.', function (done) {
       var bus = eventbus.create();
 
       assert.that(bus, is.not.sameAs(eventbus.get()));
+      done();
     });
 
-    test('returns a new instance each time.', function () {
+    test('returns a new instance each time.', function (done) {
       var bus1 = eventbus.create(),
           bus2 = eventbus.create();
 
       assert.that(bus1, is.not.sameAs(bus2));
+      done();
     });
   });
 
   suite('get', function () {
-    test('returns an event emitter.', function () {
+    test('returns an event emitter.', function (done) {
       var bus = eventbus.get();
 
       assert.that(bus, is.ofType('object'));
@@ -41,13 +44,15 @@ suite('eventbus', function () {
       assert.that(bus.emit, is.ofType('function'));
       assert.that(bus.removeListener, is.ofType('function'));
       assert.that(bus.removeAllListeners, is.ofType('function'));
+      done();
     });
 
-    test('returns always the very same instance.', function () {
+    test('returns always the very same instance.', function (done) {
       var bus1 = eventbus.get(),
           bus2 = eventbus.get();
 
       assert.that(bus1, is.sameAs(bus2));
+      done();
     });
   });
 });
